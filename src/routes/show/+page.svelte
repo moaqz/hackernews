8f37675e1c4260c7f1f97eb023ad22dc40c1a215
moaqz <goz.matu@gmail.com>
2023-06-27 11:14:59 -0300

    feat: add placeholder for future page

---
+++ src/routes/show/+page.svelte
@@ -0,0 +1,9 @@
+<div class="p-8 text-center">
+  <h1 class="text-4xl font-semibold">Coming soon</h1>
+  <p class="my-4 text-xl text-gray-300">This page is under construction</p>
+  <a
+    href="/"
+    class="rounded-md bg-yellow-400 px-3 py-1.5 text-black transition-colors hover:bg-yellow-500 focus:bg-yellow-500"
+    >Go home</a
+  >
+</div>
