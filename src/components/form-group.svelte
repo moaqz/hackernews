0f4bed7020dee561dd9202943995a9d44a0fc810
moaqz <goz.matu@gmail.com>
2023-06-27 12:25:23 -0300

    refactor: remove unnecessary style

--- src/components/form-group.svelte
+++ src/components/form-group.svelte
@@ -16,7 +16,7 @@
 <div class="flex flex-col space-y-1">
   <label for={id} class="text-zinc-300">{labelText}</label>
   <input
-    class="rounded rounded-sm border border-zinc-600 bg-zinc-700 px-2 py-1 outline-none focus:ring-2 focus:ring-zinc-500"
+    class="rounded-sm border border-zinc-600 bg-zinc-700 px-2 py-1 outline-none focus:ring-2 focus:ring-zinc-500"
     use:typeAction
     {id}
     name={id}
