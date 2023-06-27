76da8d58a17c96d4d1454122bccc1d533eafa9a1
moaqz <goz.matu@gmail.com>
2023-06-27 11:15:04 -0300

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<div class="flex flex-col items-center gap-3 py-8">
+  {#if $page.status === 404}
+    <h1 class="text-center text-3xl text-zinc-50">You must be lost.</h1>
+    <a
+      href="/"
+      class="rounded-md bg-amber-400 px-3 py-1.5 text-black transition-colors hover:bg-amber-500"
+    >
+      Go home</a
+    >
+  {:else}
+    <h1 class="text-center text-3xl text-zinc-50">
+      Whoops. We weren't expecting that
+    </h1>
+    <p class="text-xl text-zinc-300">We're investigating the issue.</p>
+  {/if}
+</div>
